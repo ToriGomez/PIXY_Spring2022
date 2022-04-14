e015fcde557b11df93355599565bd9751087951a
Victoria Gomez <77286079+ToriGomez@users.noreply.github.com>
2022-04-14 11:47:57 -0600

    Add project files.

---
+++ PIXY_Spring2022/PIXYData.Designer.vb
@@ -0,0 +1,242 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class PIXYData
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.PIXYSerialPort = New System.IO.Ports.SerialPort(Me.components)
+        Me.SendTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.ErrorLabel = New System.Windows.Forms.Label()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.PICSerialPort = New System.IO.Ports.SerialPort(Me.components)
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPageDisplay = New System.Windows.Forms.TabPage()
+        Me.Goal2Label = New System.Windows.Forms.Label()
+        Me.Goal1Label = New System.Windows.Forms.Label()
+        Me.Goal2TextBox = New System.Windows.Forms.TextBox()
+        Me.Goal1TextBox = New System.Windows.Forms.TextBox()
+        Me.TabPageTrouble = New System.Windows.Forms.TabPage()
+        Me.CommPortsLabel = New System.Windows.Forms.Label()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FILEToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HELPToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ScoresGroupBox = New System.Windows.Forms.GroupBox()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPageDisplay.SuspendLayout()
+        Me.TabPageTrouble.SuspendLayout()
+        Me.MenuStrip1.SuspendLayout()
+        Me.ScoresGroupBox.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PIXYSerialPort
+        '
+        Me.PIXYSerialPort.BaudRate = 19200
+        '
+        'ExitButton
+        '
+        Me.ExitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.ExitButton.Location = New System.Drawing.Point(15, 675)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(220, 90)
+        Me.ExitButton.TabIndex = 0
+        Me.ExitButton.Text = "&EXIT"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'ErrorLabel
+        '
+        Me.ErrorLabel.AutoSize = True
+        Me.ErrorLabel.Location = New System.Drawing.Point(1633, 839)
+        Me.ErrorLabel.Name = "ErrorLabel"
+        Me.ErrorLabel.Size = New System.Drawing.Size(0, 32)
+        Me.ErrorLabel.TabIndex = 1
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 31
+        Me.ListBox1.Location = New System.Drawing.Point(26, 50)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(270, 128)
+        Me.ListBox1.TabIndex = 2
+        '
+        'PICSerialPort
+        '
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPageDisplay)
+        Me.TabControl1.Controls.Add(Me.TabPageTrouble)
+        Me.TabControl1.Location = New System.Drawing.Point(15, 65)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(1009, 595)
+        Me.TabControl1.TabIndex = 3
+        '
+        'TabPageDisplay
+        '
+        Me.TabPageDisplay.Controls.Add(Me.ScoresGroupBox)
+        Me.TabPageDisplay.Location = New System.Drawing.Point(10, 48)
+        Me.TabPageDisplay.Name = "TabPageDisplay"
+        Me.TabPageDisplay.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPageDisplay.Size = New System.Drawing.Size(989, 537)
+        Me.TabPageDisplay.TabIndex = 0
+        Me.TabPageDisplay.Text = "DISPLAY"
+        Me.TabPageDisplay.UseVisualStyleBackColor = True
+        '
+        'Goal2Label
+        '
+        Me.Goal2Label.AutoSize = True
+        Me.Goal2Label.Location = New System.Drawing.Point(365, 63)
+        Me.Goal2Label.Name = "Goal2Label"
+        Me.Goal2Label.Size = New System.Drawing.Size(190, 32)
+        Me.Goal2Label.TabIndex = 3
+        Me.Goal2Label.Text = "Goal2: Robot "
+        '
+        'Goal1Label
+        '
+        Me.Goal1Label.AutoSize = True
+        Me.Goal1Label.Location = New System.Drawing.Point(22, 63)
+        Me.Goal1Label.Name = "Goal1Label"
+        Me.Goal1Label.Size = New System.Drawing.Size(188, 32)
+        Me.Goal1Label.TabIndex = 2
+        Me.Goal1Label.Text = "Goal1: Player"
+        '
+        'Goal2TextBox
+        '
+        Me.Goal2TextBox.Location = New System.Drawing.Point(371, 98)
+        Me.Goal2TextBox.Name = "Goal2TextBox"
+        Me.Goal2TextBox.Size = New System.Drawing.Size(230, 38)
+        Me.Goal2TextBox.TabIndex = 1
+        '
+        'Goal1TextBox
+        '
+        Me.Goal1TextBox.Location = New System.Drawing.Point(28, 98)
+        Me.Goal1TextBox.Name = "Goal1TextBox"
+        Me.Goal1TextBox.Size = New System.Drawing.Size(230, 38)
+        Me.Goal1TextBox.TabIndex = 0
+        '
+        'TabPageTrouble
+        '
+        Me.TabPageTrouble.Controls.Add(Me.CommPortsLabel)
+        Me.TabPageTrouble.Controls.Add(Me.ListBox1)
+        Me.TabPageTrouble.Location = New System.Drawing.Point(10, 48)
+        Me.TabPageTrouble.Name = "TabPageTrouble"
+        Me.TabPageTrouble.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPageTrouble.Size = New System.Drawing.Size(989, 537)
+        Me.TabPageTrouble.TabIndex = 1
+        Me.TabPageTrouble.Text = "TROUBLESHOOT"
+        Me.TabPageTrouble.UseVisualStyleBackColor = True
+        '
+        'CommPortsLabel
+        '
+        Me.CommPortsLabel.AutoSize = True
+        Me.CommPortsLabel.Location = New System.Drawing.Point(20, 15)
+        Me.CommPortsLabel.Name = "CommPortsLabel"
+        Me.CommPortsLabel.Size = New System.Drawing.Size(273, 32)
+        Me.CommPortsLabel.TabIndex = 3
+        Me.CommPortsLabel.Text = "ALL COMM PORTS:"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.GripMargin = New System.Windows.Forms.Padding(2, 2, 0, 2)
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(40, 40)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FILEToolStripMenuItem, Me.HELPToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1120, 49)
+        Me.MenuStrip1.TabIndex = 4
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FILEToolStripMenuItem
+        '
+        Me.FILEToolStripMenuItem.Name = "FILEToolStripMenuItem"
+        Me.FILEToolStripMenuItem.Size = New System.Drawing.Size(94, 45)
+        Me.FILEToolStripMenuItem.Text = "FILE"
+        '
+        'HELPToolStripMenuItem
+        '
+        Me.HELPToolStripMenuItem.Name = "HELPToolStripMenuItem"
+        Me.HELPToolStripMenuItem.Size = New System.Drawing.Size(109, 45)
+        Me.HELPToolStripMenuItem.Text = "HELP"
+        '
+        'ScoresGroupBox
+        '
+        Me.ScoresGroupBox.Controls.Add(Me.Goal1Label)
+        Me.ScoresGroupBox.Controls.Add(Me.Goal2Label)
+        Me.ScoresGroupBox.Controls.Add(Me.Goal1TextBox)
+        Me.ScoresGroupBox.Controls.Add(Me.Goal2TextBox)
+        Me.ScoresGroupBox.Location = New System.Drawing.Point(21, 50)
+        Me.ScoresGroupBox.Name = "ScoresGroupBox"
+        Me.ScoresGroupBox.Size = New System.Drawing.Size(628, 181)
+        Me.ScoresGroupBox.TabIndex = 4
+        Me.ScoresGroupBox.TabStop = False
+        Me.ScoresGroupBox.Text = "SCORES"
+        '
+        'PIXYData
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(16.0!, 31.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.CancelButton = Me.ExitButton
+        Me.ClientSize = New System.Drawing.Size(1120, 818)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Controls.Add(Me.ErrorLabel)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "PIXYData"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "AIRHOCKEY"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPageDisplay.ResumeLayout(False)
+        Me.TabPageTrouble.ResumeLayout(False)
+        Me.TabPageTrouble.PerformLayout()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ScoresGroupBox.ResumeLayout(False)
+        Me.ScoresGroupBox.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PIXYSerialPort As IO.Ports.SerialPort
+    Friend WithEvents SendTimer As Timer
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents ErrorLabel As Label
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents PICSerialPort As IO.Ports.SerialPort
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPageDisplay As TabPage
+    Friend WithEvents TabPageTrouble As TabPage
+    Friend WithEvents CommPortsLabel As Label
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FILEToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HELPToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Goal2Label As Label
+    Friend WithEvents Goal1Label As Label
+    Friend WithEvents Goal2TextBox As TextBox
+    Friend WithEvents Goal1TextBox As TextBox
+    Friend WithEvents ScoresGroupBox As GroupBox
+End Class
---
+++ PIXY_Spring2022/PIXYData.vb
@@ -0,0 +1,137 @@
+﻿'Tori Gomez
+'3/30/21
+'Code for the PIXYCMUcam5 to get data to be sent to the PIC16LF1788.
+'The PIXY will detect the puck that is illuminated in green and the striker.
+'Based on the serial data sent from the PIXY, this code will read and analyze
+'and tell the PIC16LF1788 on what to do in direction the puck will need to move.
+
+Option Strict On
+Option Explicit On
+Imports System.IO.Ports
+Imports System.Math
+Imports System.Threading.Thread
+
+Public Class PIXYData
+    Dim receiveBytesPic(4) As Byte                                                                  'All the Bytes of data the PIC will send
+    Dim recieveBytesPixy(15) As Byte                                                                'All the Bytes of data the PIXY will send
+    Dim comPort1String As String                                                                    'Both of the Serial Ports used in the program
+    Dim comPort2String As String                                                                    '/
+
+    'Sub for when the Form loads, test and opens all of the communication ports. One for the PIXY data through the breakout board and the
+    'main board from the PIC16LF1788.
+    Private Sub PIXYData_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        GetSerialPortNames()                                                                        'Sub to get all the ports being used
+
+
+
+        PICSerialPort.PortName = comPort1String                                                     'Test if the port is the main PIC
+        PICSerialPort.BaudRate = 57600                                                              'Baudrate of 57600 bits/sec
+        PICSerialPort.DataBits = 8                                                                  '8 data bits
+        PICSerialPort.StopBits = IO.Ports.StopBits.One                                              '1 stop bit
+        PICSerialPort.Parity = IO.Ports.Parity.None                                                 'No parity bits
+        PICSerialPort.Open()
+
+
+
+        PIXYSerialPort.PortName = comPort2String                                                    'Test if the port is the PIXY
+        PIXYSerialPort.BaudRate = 19200                                                             'Baudrate of 9600 bits/sec
+        PIXYSerialPort.DataBits = 8                                                                 '8 data bits
+        PIXYSerialPort.StopBits = IO.Ports.StopBits.One                                             '1 stop bit
+        PIXYSerialPort.Parity = IO.Ports.Parity.None                                                'No parity bits
+        PIXYSerialPort.Open()
+
+
+        SendTimer.Enabled = True                                                                       'Enables the timer
+    End Sub
+
+
+    'Sub of the Exit button that closes out the program.
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        PICSerialPort.Close()                                                                       'Shut off ports when form closed
+        PIXYSerialPort.Close()                                                                      '/
+        Me.Close()                                                                                  'Close Program
+    End Sub
+
+
+    'Show all available/in-use COMM ports.
+    Sub GetSerialPortNames()
+        Dim comPortInteger As Integer = 1
+        For Each sp As String In My.Computer.Ports.SerialPortNames
+            If comPortInteger = 1 Then
+                comPort1String = sp
+                comPortInteger += 1
+            Else
+                comPort2String = sp
+            End If
+            ListBox1.Items.Add(sp)
+        Next
+    End Sub
+
+
+    Function PixyRawSerial() As Boolean
+        Dim PixyBoolean As Boolean = True
+        Dim byte0Integer As Integer
+        Dim byte1Integer As Integer
+        Dim byte2Integer As Integer
+        Dim byte3Integer As Integer
+        Dim byte4Integer As Integer
+        Dim byte5Integer As Integer
+        Dim byte6Integer As Integer
+        Dim byte7Integer As Integer
+        Dim normObjectInteger As Integer
+        Dim codeObjectInteger As Integer
+        Dim checksumInteger As Integer
+        Dim xInteger As Integer
+        Dim yInteger As Integer
+
+        CheckForIllegalCrossThreadCalls = False
+        byte0Integer = PIXYSerialPort.ReadByte                                                    'Sets the receivedHeader to the value of the first byte sent from the PIC16F883
+        normObjectInteger = byte0Integer
+        byte1Integer = PIXYSerialPort.ReadByte
+        normObjectInteger += byte1Integer
+        If normObjectInteger = 43605 Then                                                              'If the PIXY sends the first sync bytes of 0xAA55, indicating normal object
+            byte2Integer = PIXYSerialPort.ReadByte
+            codeObjectInteger = byte2Integer
+            byte3Integer = PIXYSerialPort.ReadByte
+            codeObjectInteger += byte3Integer
+            If codeObjectInteger = 43606 Then                                                           'If the PIXY sends the second sync bytes of 0xAA56, indicating color code object
+
+                PixyBoolean = True
+
+                byte4Integer = PIXYSerialPort.ReadByte                                                     'Receive High Byte
+                checksumInteger = byte4Integer
+                byte5Integer = PIXYSerialPort.ReadByte                                                     'Receive Low Byte
+                checksumInteger += byte5Integer
+
+                'adHighByteInteger = adHighByteInteger * 4                                                   'Left shift 2 places
+                'adLowByteInteger = adLowByteInteger \ 64                                                    'Right shift 6 places
+                'adInteger = adHighByteInteger + adLowByteInteger                                            'Add values together for total number
+                'CountTextBox.Text = (CInt(adInteger).ToString)                                              '0 to 1023 count value                                                                        
+
+            End If
+        End If
+        Return PixyBoolean
+    End Function
+
+
+    'Sub for read the serial data from the PIC16LF1788 which will send 3 bytes, receive up to 5 bytes of data in case of errors.
+    Private Sub PICSerialPort_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles PICSerialPort.DataReceived
+        Dim receivedHeaderInteger As Integer                                                                    'Header to verify the serial port is the PIC
+
+        PICSerialPort.Read(receiveBytesPic, 0, 5)                                                               'Save serial
+        CheckForIllegalCrossThreadCalls = False
+        receivedHeaderInteger = PICSerialPort.ReadByte                                                          'Sets the receivedHeader to the values of the first byte sent from the PIC
+        If Not receivedHeaderInteger = 36 Then                                                                  'Test if the correct port is selected
+            PICSerialPort.PortName = comPort2String                                                             'If no header Of 0x24/$ Then re-name port
+            PIXYSerialPort.PortName = comPort1String                                                            'Re-name the PIXY to the new port
+        End If
+    End Sub
+
+
+
+    'Sub for read the serial data from the PIC16LF1788 which will send 14 bytes, receive up to 15 bytes of data in case of errors.
+    Private Sub PIXYSerialPort_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles PIXYSerialPort.DataReceived
+        PIXYSerialPort.Read(recieveBytesPixy, 0, 15)                                                            'Save serial
+    End Sub
+
+End Class
